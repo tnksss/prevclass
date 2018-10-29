60e1783c2d3cdb45f7bccee6e9cba93fb1c6451c
Tnksss <robsons@alunos.utfpr.edu.br>
2018-10-29 16:19:06 -0300

    card para turma

--- resources/views/teacher/home.blade.php
+++ resources/views/teacher/home.blade.php
@@ -6,10 +6,29 @@
     </section>
     <section class="content">
         <p>You are logged in!</p>
+        @if($teacher->has('supplies'))
+        <div class="row">
+            
+    
         @foreach ($teacher->supplies as $supply)
-            <li>{{$supply->grade->name}}</li>
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-aqua">
+                    <div class="inner">
+                    <h3><li>{{$supply->grade->degree}} {{$supply->grade->order}}</li></h3>
+        
+                        <p>New Orders</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>     
             
         @endforeach
+        </div>
+        @endif
     </section>
 @endsection
 
