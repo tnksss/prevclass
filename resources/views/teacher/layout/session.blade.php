e476c07134268a51107fa5b17342a68f6dcdf45c
Tnks <robsons@alunos.utfpr.edu.br>
2018-11-13 17:14:30 -0200

    shim movido

--- resources/views/teacher/layout/session.blade.php
+++ resources/views/teacher/layout/session.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    @include('admin.partials.shim')
+    @include('layouts.shim')
 
     <title>PrevClass - Login Professor</title>
 
