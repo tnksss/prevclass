2fdcb3c0ea9e95a2d3baf2113a676e39167c1e78
Tnks <robsons@alunos.utfpr.edu.br>
2018-11-19 19:45:56 -0200

    cards admin dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Users\Manager;
+use App\Models\Unity;
+use App\Models\Supply;
+use App\Models\Student;
+use App\Models\Enrollment;
+use App\Models\Users\User;
 
 
 class AdminController extends Controller
@@ -26,7 +31,20 @@ class AdminController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $unities = Unity::all();
+        $teachers = User::all();
+        $supplies = Supply::all();
+        $students = Student::all();
+        $enrollments = Enrollment::all();
+        $managers = Manager::all();
+        return view('admin.home', compact([
+            'unities',
+            'teachers',
+            'supplies',
+            'students',
+            'enrollments',
+            'managers',
+        ]));
     }
     public function managers()
     {
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,109 @@
 @extends('admin.layout.app')
-
+@section('content_header')
+<h1>Dashboard</h1>
+@stop
 @section('content')
-    <section class="content-header">
-        <h1>Dashboard</h1>
-    </section>
-    <section class="content">
-        <p>You are logged in!</p>
-    </section>
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-body">
+                <div class="col-md-6">
+                    <div class="info-box bg-aqua">
+                        <span class="info-box-icon"><i class="fa fa-institution"></i></span>                        
+                        <div class="info-box-content">
+                            <span class="info-box-text">Unidades</span>
+                            <span class="info-box-number">{{$unities->count()}}</span>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 40%"></div>
+                            </div>
+                            <span class="progress-description">
+                                40% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="info-box bg-green">
+                        <span class="info-box-icon"><i class="fa fa-pencil"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Secretários</span>
+                            <span class="info-box-number">{{$teachers->count()}}</span>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 20%"></div>
+                            </div>
+                            <span class="progress-description">
+                                20% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="info-box bg-red">
+                        <span class="info-box-icon"><i class="fa fa-archive"></i></span>
+                        
+                        <div class="info-box-content">
+                            <span class="info-box-text">Matrículas</span>
+                            <span class="info-box-number">{{$enrollments->count()}}</span>
+                            
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 70%"></div>
+                            </div>
+                            <span class="progress-description">
+                                70% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="info-box bg-yellow">
+                        <span class="info-box-icon"><i class="fa fa-briefcase"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Suprimentos</span>
+                            <span class="info-box-number">{{$supplies->count()}}</span>
+                            
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 50%"></div>
+                            </div>
+                            <span class="progress-description">
+                                50% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="info-box bg-navy">
+                        <span class="info-box-icon"><i class="fa fa-mortar-board"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Professores</span>
+                            <span class="info-box-number">{{$managers->count()}}</span>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 70%"></div>
+                            </div>
+                            <span class="progress-description">
+                                70% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="info-box bg-purple">
+                        <span class="info-box-icon"><i class="fa fa-child"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Alunos</span>
+                            <span class="info-box-number">{{$students->count()}}</span>
+                            <div class="progress">
+                                <div class="progress-bar" style="width: 50%"></div>
+                            </div>
+                            <span class="progress-description">
+                                50% Increase in 30 Days
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
